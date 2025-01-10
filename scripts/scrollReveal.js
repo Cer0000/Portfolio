4991a5c735b6ed13fc07b5f01e6ab68429377eee
Nicolás Krukowski <nicokrukowski@gmail.com>
2025-01-10 20:13:52 -0300

    Agregando archivos

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.inicio', { duration: 1500, reset: true});
+
+ScrollReveal().reveal('.presentacion', { duration: 1500, reset: true});
+
+ScrollReveal().reveal('.card-habilidades', { duration: 1000, reset: true});
+
+ScrollReveal().reveal('.card-contacto', { duration: 1000, reset: true});
+
+ScrollReveal().reveal('.img-anim', { interval: 200, reset: true });
---
+++ scripts/typeitTest.js
@@ -0,0 +1,14 @@
+new TypeIt("#intro", {
+  strings: ["Nicolás Krukowski Developer"],
+  startDelay: 500,
+  speed: 100,
+  cursorChar: "_",
+  waitUntilVisible: true,
+}).go();
+
+new TypeIt("#presentacion", {
+  startDelay: 3000,
+  speed: 30,
+  cursorChar: "_",
+  waitUntilVisible: true,
+}).go();
